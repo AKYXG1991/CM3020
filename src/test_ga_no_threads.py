35d04db220119de1ecc5bc228f73db2267fe4474
AKYXG1991 <84016858+AKYXG1991@users.noreply.github.com>
2025-06-18 18:06:12 +0800

    Add sandbox environment support

--- src/test_ga_no_threads.py
+++ src/test_ga_no_threads.py
@@ -1,8 +1,8 @@
-# If you on a Windows machine with any Python version 
+# If you on a Windows machine with any Python version
 # or an M1 mac with any Python version
 # or an Intel Mac with Python > 3.7
 # the multi-threaded version does not work
-# so instead, you can use this version. 
+# so instead, you can use this version.
 
 import unittest
 import population
@@ -13,25 +13,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class TestGA(unittest.TestCase):
     def testBasicGA(self):
-        pop = population.Population(pop_size=10, 
-                                    gene_count=3)
-        #sim = simulation.ThreadedSim(pool_size=1)
-        sim = simulation.Simulation()
+        pop = population.Population(pop_size=10, gene_count=3)
+        sim = simulation.Simulation(use_gui=True, use_sandbox=True)
         records = []
 
         for iteration in range(1000):
-            # this is a non-threaded version 
-            # where we just call run_creature instead
-            # of eval_population
             for cr in pop.creatures:
-                sim.run_creature(cr, 2400)            
-            #sim.eval_population(pop, 2400)
-            fits = [cr.get_height_climbed()
-                    for cr in pop.creatures]
-            links = [len(cr.get_expanded_links()) 
-                    for cr in pop.creatures]
+                sim.run_creature(cr, 2400)
+            fits = [cr.get_height_climbed() for cr in pop.creatures]
+            links = [len(cr.get_expanded_links()) for cr in pop.creatures]
             max_fit = float(np.max(fits))
             mean_fit = float(np.mean(fits))
             mean_link = float(np.mean(links))
@@ -52,7 +45,6 @@ class TestGA(unittest.TestCase):
                 p2_ind = population.Population.select_parent(fit_map)
                 p1 = pop.creatures[p1_ind]
                 p2 = pop.creatures[p2_ind]
-                # now we have the parents!
                 dna = genome.Genome.crossover(p1.dna, p2.dna)
                 dna = genome.Genome.point_mutate(dna, rate=0.1, amount=0.25)
                 dna = genome.Genome.shrink_mutate(dna, rate=0.25)
@@ -60,17 +52,16 @@ class TestGA(unittest.TestCase):
                 cr = creature.Creature(1)
                 cr.update_dna(dna)
                 new_creatures.append(cr)
-            # elitism
             max_fit = np.max(fits)
             for cr in pop.creatures:
                 if cr.get_height_climbed() == max_fit:
                     new_cr = creature.Creature(1)
                     new_cr.update_dna(cr.dna)
                     new_creatures[0] = new_cr
-                    filename = "elite_"+str(iteration)+".csv"
+                    filename = "elite_" + str(iteration) + ".csv"
                     genome.Genome.to_csv(cr.dna, filename)
                     break
-            
+
             pop.creatures = new_creatures
 
         df = pd.DataFrame(records)
@@ -100,4 +91,6 @@ class TestGA(unittest.TestCase):
 
         self.assertNotEqual(fits[0], 0)
 
-unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
